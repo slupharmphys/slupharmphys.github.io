78a3dec3a09844d6f33eace3cbc3309e7632393e
zhangj5 <jinsongzhang@slu.edu>
2020-09-15 17:54:53 -0500

    3rd commit

---
+++ jpg_url.sh
@@ -0,0 +1,27 @@
+#! /bin/sh
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1362.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1385.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1386.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1389.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1423.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1467.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1581.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1585.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1592.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1630.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1645.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1654.jpg
+
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://physicians.slucare.edu/Custom/Photos/1680.jpg
---
+++ wget.sh
@@ -0,0 +1,13 @@
+wget -e robots=off -r parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1389/michael-ariel?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1680/anutosh-chakraborty?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1467/john-chrivia?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1654/ian_mitchelle-de_vera?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1386/terrance-egan?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1585/colin-flaveny?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1423/mark-knuepfer?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1630/daniela-salvemini?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1385/willis-samson?OrgUnits=619&SetSize=24 
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1592/john-walker?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1645/gina-yosten?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1362/daniel-zahm?OrgUnits=619&SetSize=24
+wget -e robots=off -r --page-requisites --no-parent --no-clobber --wait=20 --limit-rate=20K https://medfaculty.slu.edu/details/1581/jinsong-zhang?OrgUnits=619&SetSize=24
